fef295b85b060104b86c049c2532495054bf759b
Hasan Askari <hasanaskarii112@gmail.com>
2022-10-19 02:41:58 +0500

    REST API for goals

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  PRIVATE
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Get goals` });
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 // @desc    Set goals
@@ -15,7 +18,9 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Please add a text field');
     }
-
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
     res.status(200).json({ message: `Set goals` });
 });
 
@@ -23,6 +28,17 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   PUT /api/goal/:id
 // @access  PRIVATE
 const updateGoal = asyncHandler(async (req, res) => {
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
     res.status(200).json({ message: `Update goal ${req.params.id}` })
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const { application } = require('express');
 const express = require('express');
-const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use(express.json());
