e99ce34e175d747160aefa6eee61459bd7207181
Hasan Askari <hasanaskarii112@gmail.com>
2022-10-22 02:53:51 +0500

    fixed update and delete controllers

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -58,14 +58,23 @@ const updateGoal = asyncHandler(async (req, res) => {
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
         new: true
     })
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+    res.status(200).json(updatedGoal);
 });
 
 // @desc    delete goal 
 // @route   DELETE /api/goals/:id
 // @access  PRIVATE
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    await goal.remove();
+
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
