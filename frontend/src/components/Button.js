827a6797df8434bd9c82307259d6c32ddc35d41e
Hasan Askari <hasanaskarii112@gmail.com>
2022-11-23 01:02:40 +0500

    Minor progress

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div>
+        <button className='btn'>Add</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,28 @@
+import propTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: propTypes.string.isRequired,
+}
+
+
+// CSS in JS
+// const Style = {
+//   color: 'red',
+//   backgroundColor: "black",
+//   width: "30px"
+// }
+export default Header
