9e4545c2350ff97698fda32b911b91d6209e236e
Hasan Askari <hasanaskarii112@gmail.com>
2022-11-24 00:04:52 +0500

    minor progress

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,14 @@
-const Button = () => {
-  return (
-    <div>
-        <button className='btn'>Add</button>
-    </div>
-  )
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text }) => {
+  return <button style={{backgroundColor: color}} className='btn'>{text}</button>
+}
+Button.defaultProps = {
+  color: 'steelblue'
 }
 
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+}
 export default Button
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ title }) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button />
+        <Button color='green' text='Add'/>
     </header>
   )
 }
